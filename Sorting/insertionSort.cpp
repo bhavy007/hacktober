c0b5bd406d5adef06429e1578422f9a77f7ff71e
bhavy007 <44473716+bhavy007@users.noreply.github.com>
2022-10-04 02:09:18 +0530

    Insertion Sort

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -1,19 +1,44 @@
-//in-Place Algorithm and stable 
-//worst-case time complexity: O(n^2) in reverse sorted array case
-//best case time complexity: O(n) in already sorted array case
-//in general: O(n^2)
 
-void insertionSort(int arr[],int n)
-{
-    for(int i=1; i<n; i++)
-    {
-        int key=arr[i];
-        int j=i-1;
-        while(j>=0 && arr[i]>key)
-        {
-            arr[j+1]=arr[j];
-            j--;
-        }
-        arr[j+1]=key;
-    }
+#include <iostream>
+
+using namespace std;
+
+
+
+
+void InsertionSort(int a[100], int n){
+    int value,hole;
+  for(int i=0;i<n;i++){
+      value=a[i];
+      hole = i;
+      while(hole>0 && a[hole-1]>value){
+          a[hole]=a[hole-1];
+          hole--;
+      }
+      a[hole]=value;
+  }
 }
+
+int main(){
+
+  int x;
+  printf("\nEnter size of array: ");
+  scanf("%d",&x);
+  int arr[x];
+  printf("\nEnter elements of array: ");
+  for(int i=0;i<x;i++){
+    scanf("%d",&arr[i]);
+  }
+  InsertionSort(arr,x);
+  printf("\nSorted array: ");
+  
+  
+  for(int i=0;i<x;i++){
+    printf("%d ",arr[i]);
+  }
+
+
+  return 0;
+}
+
+
